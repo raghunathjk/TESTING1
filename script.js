0c8106a80e63d1cd49ec212c3a95f8736e34f77c
raghunathjk <raghunath.jayakashinath@gmail.com>
2022-05-27 17:53:01 +0530

    PUSH 4

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.addEventListener("load", () => {
 
     const track_content_el = document.createElement("div");
     track_content_el.classList.add("content");
-    track_content_el.innerText = track;
+    // task_content_el.innerText = track;
 
     track_el.appendChild(track_content_el);
 
@@ -26,9 +26,43 @@ window.addEventListener("load", () => {
     track_input_el.classList.add("text");
     track_input_el.type = "text";
     track_input_el.value = track;
+    track_input_el.setAttribute("readOnly", "readOnly");
 
     track_content_el.appendChild(track_input_el);
 
+    const track_actions_el = document.createElement("div");
+    track_actions_el.classList.add("actions");
+
+    const track_edit_el = document.createElement("button");
+    task_edit_el.classList.add("edit");
+    track_edit_el.innerText = "Edit";
+
+    const track_delete_el = document.createElement("button");
+    track_delete_el.classList.add("delete");
+    track_delete_el.innerText = "Delete";
+
+    track_actions_el.appendChild(track_edit_el);
+    track_actions_el.appendChild(track_delete_el);
+
+    track_el.appendChild(track_actions_el);
+
     list_el.appendChild(track_el);
+
+    input.value = "";
+
+    track_edit_el.addEventListener("click", () => {
+      if (track_edit_el.innerText.toLowerCase() == "edit") {
+        track_input_el.removeAttribute("readOnly");
+        track_input_el.focus();
+        track_edi_el.innerText = "Save";
+      } else {
+        track_input_el.setAttribute("readOnly", "readOnly");
+        track_input_el.innerText = "Edit";
+      }
+    });
+
+    track_delete_el.addEventListener("click", () => {
+      list_el.removeChild(track_el);
+    });
   });
 });
